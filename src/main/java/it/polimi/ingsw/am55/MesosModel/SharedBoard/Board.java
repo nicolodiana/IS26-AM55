8f106f30396f8fd08ef151a272f935bb0123d8d2
giacomocagnazzo <giacomo.cagnazzo@mail.polimi.it>
2026-05-05 14:33:40 +0200

    removed EmptytribeDeckException

--- src/main/java/it/polimi/ingsw/am55/MesosModel/SharedBoard/Board.java
+++ src/main/java/it/polimi/ingsw/am55/MesosModel/SharedBoard/Board.java
@@ -8,7 +8,6 @@ import it.polimi.ingsw.am55.MesosModel.Decks.BuildingDeck;
 import it.polimi.ingsw.am55.MesosModel.Decks.TribeDeck;
 import it.polimi.ingsw.am55.MesosModel.Enum.RowType;
 import it.polimi.ingsw.am55.MesosModel.Exceptions.BiddingTicketIsTaken;
-import it.polimi.ingsw.am55.MesosModel.Exceptions.EmptyTribeDeckException;
 import it.polimi.ingsw.am55.MesosModel.Player.Player;
 import it.polimi.ingsw.am55.dto.BiddingTicketView;
 import it.polimi.ingsw.am55.dto.BoardView;
@@ -125,7 +124,7 @@ public class Board {
     }
 
     //after all the event in order to restore the board
-    public boolean restoreForRound(int numPlayers) throws EmptyTribeDeckException{
+    public boolean restoreForRound(int numPlayers) {
         lowerRow.clearRoundEnd();
         lowerRow.swapTribeRow(upperRow);
         TribeCard tmp;
