dcd9bed04699beff43dd2f58ba15c0e82377dfc1
nicoa <nico.diana@icloud.com>
2026-06-13 19:35:42 +0200

    backup-rete-preincontro

--- src/main/java/it/polimi/ingsw/am55/Server.java
+++ src/main/java/it/polimi/ingsw/am55/Server.java
@@ -8,6 +8,10 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.ExportException;
 
+/**gg
+ *
+ *
+ */
 public class Server {
 
     private static final int DEFAULT_RMI_PORT = 1234;
